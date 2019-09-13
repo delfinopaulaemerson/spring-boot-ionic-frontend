7b6f839cb188f1590bbee60bab49c96aac9a3d5b
delfinopaulaemerson <delfs@hotmail.com>
2019-09-13 16:17:24 -0300

    incluindo o token nas requisições

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -1,26 +1,38 @@
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from "rxjs";
+import { StorageService } from "../services/storage.services";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-
+    constructor(public storage: StorageService) {
+    }
+    
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        console.log('Teste interceptor');
-        return next.handle(req)
-        .catch((error, caught) => {
-            let errorObj = error;
-            if (errorObj.error) {
-                errorObj = errorObj.error;
-            }
-            if(!errorObj.status){
-               errorObj = JSON.parse(errorObj);     
-            }
+        let localUser = this.storage.getLocalUser();
+        
+        if (localUser) {
+            const authReq = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + localUser.token)});
+            return next.handle(authReq)
+        }else{
+            console.log('Teste interceptor');
+            return next.handle(req)
+            .catch((error, caught) => {
+                let errorObj = error;
+                if (errorObj.error) {
+                    errorObj = errorObj.error;
+                }
+                if(!errorObj.status){
+                   errorObj = JSON.parse(errorObj);     
+                }
+    
+                console.log("Erro detectado pelo Interceptors:");
+                console.log(errorObj);
+                return Observable.throw(errorObj);        
+            }) as any;
+        }
 
-            console.log("Erro detectado pelo Interceptors:");
-            console.log(errorObj);
-            return Observable.throw(errorObj);        
-        }) as any;
+       
     }
  
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,17 +1,14 @@
-import { StorageService } from './../storage.services';
 import { ClienteDTO } from './../../models/cliente.dto';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class ClienteService{
 
-    constructor(public http:HttpClient, public storage: StorageService){}
+    constructor(public http:HttpClient){}
 
     findByEmail(email:string) : Observable<ClienteDTO>{
-        let token = this.storage.getLocalUser().token;
-        let authHeaders = new HttpHeaders({'Authorization': 'Bearer '+ token});
-        return this.http.get<ClienteDTO>('http://localhost:8080/clientes/email?value='+email,{headers: authHeaders});
+        return this.http.get<ClienteDTO>('http://localhost:8080/clientes/email?value='+email);
     }
 }
