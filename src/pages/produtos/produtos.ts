f29f3484b265e84c436ad46748c64a6c45ee7b3a
delfinopaulaemerson <delfs@hotmail.com>
2019-09-15 09:46:56 -0300

    Componente loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,7 +2,7 @@ import { ProdutodetailPage } from './../produtodetail/produtodetail';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { CategoriasPage } from '../categorias/categorias';
 
 
@@ -15,12 +15,16 @@ export class ProdutosPage {
 
   items : ProdutoDTO[];
 
-  constructor(public alertCtrl:AlertController,public navCtrl: NavController, public navParams: NavParams, public service: ProdutoService) {
+  constructor(public alertCtrl:AlertController,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public service: ProdutoService,
+    public loadcontrol:LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get('categoria_id');
-    
+    let loading = this.presentLoading();
     if(categoria_id == undefined){
       let alert = this.alertCtrl.create({
         title: 'Categorias',
@@ -34,11 +38,20 @@ export class ProdutosPage {
 
     this.service.findByCategoria(categoria_id).subscribe(response =>{
       this.items = response['content'];
+      loading.dismiss();
     });
   }
 
   showDetail(produto_id:string){
     this.navCtrl.push(ProdutodetailPage,{produto_id:produto_id});
+  }
+
+  presentLoading() {
+    let loader = this.loadcontrol.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
   }
 
 }
