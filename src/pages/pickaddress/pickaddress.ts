3d028fe9bc739ebec667e2251b6653448b750843
delfinopaulaemerson <delfs@hotmail.com>
2019-09-15 07:25:32 -0300

    Armazenamento dos dados do pedido

--- src/pages/pickaddress/pickaddress.ts
+++ src/pages/pickaddress/pickaddress.ts
@@ -1,3 +1,5 @@
+import { CartService } from './../../services/domain/cart.service';
+import { PedidoDTO } from './../../models/pedido.dto';
 import { StorageService } from './../../services/storage.services';
 import { ClienteService } from './../../services/domain/cliente.service';
 import { Component } from '@angular/core';
@@ -12,8 +14,9 @@ import { HomePage } from '../home/home';
 })
 export class PickaddressPage {
 items : EnderecoDTO[];
+pedido:PedidoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,public storage:StorageService, public service : ClienteService) {
+  constructor(public cartservice:CartService,public navCtrl: NavController, public navParams: NavParams,public storage:StorageService, public service : ClienteService) {
   }
 
   ionViewDidLoad() {
@@ -21,12 +24,22 @@ items : EnderecoDTO[];
     if(localUser && localUser.email){
          this.service.findByEmail(localUser.email).subscribe(response => {
            this.items = response['enderecos'];
+           let cart = this.cartservice.getCart();
+           this.pedido = {
+             cliente: {id: response['id']},
+             enderecoDeEntrega:null,
+             pagamento:null,
+             itens : cart.items.map(x => {return {quantidade: x.quantidade, produto: {id: x.produto.id}}})
+           };
+
          });
     }
    }
 
   nextPage(item: EnderecoDTO){
-
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido);
+    //this.navCtrl.push('PaymentPage', {pedido: this.pedido});
   }
 
 }
