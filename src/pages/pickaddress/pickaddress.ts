d522003bff48e8f4c6b0407dc5565d0debad2c61
delfinopaulaemerson <delfs@hotmail.com>
2019-09-15 06:46:47 -0300

    Tela de escolha de endereço

--- src/pages/pickaddress/pickaddress.ts
+++ src/pages/pickaddress/pickaddress.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the PickaddressPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { EnderecoDTO } from '../../models/endereco.dto';
 
 @IonicPage()
 @Component({
@@ -14,12 +8,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'pickaddress.html',
 })
 export class PickaddressPage {
+items : EnderecoDTO[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad PickaddressPage');
+    this.items =[ {
+      id:"1",
+      logradouro:"rua a",
+      numero:"4",
+      complemento:"casa",
+      bairro:"são cristovão",
+      cep:"20921080",
+      cidade : {
+        id:"1",
+        nome:"Uberlandia",
+        estado :{
+          id:"1",
+          nome:"Minas Gerais"
+        }
+      }
+    },{
+      id:"2",
+      logradouro:"rua b",
+      numero:"4",
+      complemento:"apartamento",
+      bairro:"copacabana",
+      cep:"20921000",
+      cidade : {
+        id:"2",
+        nome:"Rio de janeiro",
+        estado :{
+          id:"2",
+          nome:"Rio de Janeiro"
+        }
+      }
+    }];
+   
+  }
+
+  nextPage(Endereco: EnderecoDTO){
+
   }
 
 }
